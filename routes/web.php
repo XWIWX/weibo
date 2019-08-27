8c9757b3f008bac072d1a05a41ea265a647aa7b2
王立鹏 <4110398@qq.com>
2019-08-27 15:48:29 +0800

    访问策略

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/about', 'StaticPagesController@about')->name('action');
 Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users','UsersController');
-//Route::get('users/{user}', 'UsersController@show')->name('users.show');
+Route::get('users/{user}', 'UsersController@show')->name('users.show');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
